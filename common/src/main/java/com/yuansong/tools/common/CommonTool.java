35cc6fb3bd6c5c7ca23ad7d470f511ef211de7d3
yuansong6 <yuansong6@163.com>
2020-01-09 18:01:34 +0800

    MD5计算参数修改，有String改为byte[]

--- common/src/main/java/com/yuansong/tools/common/CommonTool.java
+++ common/src/main/java/com/yuansong/tools/common/CommonTool.java
@@ -18,14 +18,15 @@ public class CommonTool {
 	 * @throws NoSuchAlgorithmException 
 	 * @throws UnsupportedEncodingException 
 	 */
-	public String Md5Encode(String data) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+	public String Md5Encode(byte[] data) throws NoSuchAlgorithmException, UnsupportedEncodingException{
 	    MessageDigest md5 = null;	    
         md5 = MessageDigest.getInstance("MD5");
 		
-		byte[] byteArray = null;
-		byteArray = data.getBytes("UTF-8");
-		
-		byte[] md5Bytes = md5.digest(byteArray);
+//		byte[] byteArray = null;
+//		byteArray = data.getBytes("UTF-8");		
+//		byte[] md5Bytes = md5.digest(byteArray);
+        
+		byte[] md5Bytes = md5.digest(data);
 		StringBuffer hexValue = new StringBuffer();
 		for (int i = 0; i < md5Bytes.length; i++) {
 		    int val = ((int) md5Bytes[i]) & 0xff;
